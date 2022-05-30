1894ec51f63b188524accc556793e191cc47e67d
dogukandurkaya <98876145+dogukandurkaya@users.noreply.github.com>
2022-05-30 13:09:35 +0300

    30.05.2022

--- DurkayaRecipe/DurkayaRecipe.WebUI/Models/CategoryModel.cs
+++ DurkayaRecipe/DurkayaRecipe.WebUI/Models/CategoryModel.cs
@@ -1,6 +1,7 @@
 ﻿using DurkayaRecipe.Entity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +10,9 @@ namespace DurkayaRecipe.WebUI.Models
     public class CategoryModel
     {
         public int CategoryId { get; set; }
+
+        [Required(ErrorMessage = "Yemek ismi zorunludur!")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Ürün ismi 5-50 karakter uzunluğunda olmalıdır!")]
         public string CategoryName { get; set; }
         public string CategoryDescription { get; set; }
         public string Url { get; set; }
