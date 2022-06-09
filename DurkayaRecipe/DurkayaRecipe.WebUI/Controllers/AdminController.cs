a3f131034fa3df841faddf39b7882d4499b29d65
dogukandurkaya <98876145+dogukandurkaya@users.noreply.github.com>
2022-06-09 10:48:09 +0300

    09.06.2022

--- DurkayaRecipe/DurkayaRecipe.WebUI/Controllers/AdminController.cs
+++ DurkayaRecipe/DurkayaRecipe.WebUI/Controllers/AdminController.cs
@@ -11,6 +11,8 @@ using System.IO;
 using System.Linq;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using DurkayaRecipe.WebUI.Identity;
 
 namespace DurkayaRecipe.WebUI.Controllers
 {
@@ -18,10 +20,218 @@ namespace DurkayaRecipe.WebUI.Controllers
     {
         private readonly IFoodService _foodService;
         private readonly ICategoryService _categoryService;
-        public AdminController(IFoodService foodService, ICategoryService categoryService)
+        private RoleManager<IdentityRole> _roleManager;
+        private UserManager<User> _userManager;
+
+        public AdminController(IFoodService foodService, ICategoryService categoryService, RoleManager<IdentityRole> roleManager, UserManager<User> userManager)
         {
             _foodService = foodService;
             _categoryService = categoryService;
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+        public IActionResult UserList()
+        {
+            return View(_userManager.Users);
+        }
+        public IActionResult UserCreate()
+        {
+            var roles = _roleManager.Roles.Select(i => i.Name);
+            ViewBag.Roles = roles;
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> UserCreate(UserDetailsModel model, string[] selectedRoles)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new User()
+                {
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    UserName = model.UserName,
+                    Email = model.Email,
+                    EmailConfirmed = model.EmailConfirmed
+                };
+                var result = await _userManager.CreateAsync(user, "Qwe123.");
+                if (result.Succeeded)
+                {
+                    selectedRoles = selectedRoles ?? new string[] { };
+                    await _userManager.AddToRolesAsync(user, selectedRoles);
+                    return Redirect("~/admin/user/list");
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+                var roles = _roleManager.Roles.Select(i => i.Name);
+                ViewBag.Roles = roles;
+                return View(model);
+            }
+            var roles2 = _roleManager.Roles.Select(i => i.Name);
+            ViewBag.Roles = roles2;
+            return View(model);
+        }
+        public async Task<IActionResult> UserEdit(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                var selectedRoles = await _userManager.GetRolesAsync(user);
+                var roles = _roleManager.Roles.Select(i => i.Name);
+                ViewBag.Roles = roles;
+                return View(new UserDetailsModel()
+                {
+                    UserId = user.Id,
+                    FirstName = user.FirstName,
+                    LastName = user.LastName,
+                    UserName = user.UserName,
+                    Email = user.Email,
+                    EmailConfirmed = user.EmailConfirmed,
+                    SelectedRoles = selectedRoles
+                });
+            }
+            return Redirect("~/admin/user/list");
+        }
+        [HttpPost]
+        public async Task<IActionResult> UserEdit(UserDetailsModel model, string[] selectedRoles)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByIdAsync(model.UserId);
+                if (user != null)
+                {
+                    user.FirstName = model.FirstName;
+                    user.LastName = model.LastName;
+                    user.UserName = model.UserName;
+                    user.Email = model.Email;
+                    user.EmailConfirmed = model.EmailConfirmed;
+
+                    var result = await _userManager.UpdateAsync(user);
+                    if (result.Succeeded)
+                    {
+                        var userRoles = await _userManager.GetRolesAsync(user);
+                        selectedRoles = selectedRoles ?? new string[] { };
+                        await _userManager.AddToRolesAsync(user, selectedRoles.Except(userRoles).ToArray<string>());
+                        await _userManager.RemoveFromRolesAsync(user, userRoles.Except(selectedRoles).ToArray<string>());
+                        return Redirect("~/admin/user/list");
+                    }
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                    var roles = _roleManager.Roles.Select(i => i.Name);
+                    ViewBag.Roles = roles;
+                    return View(model);
+                }
+                ModelState.AddModelError("", "Böyle bir kullanıcı yok!");
+                var roles2 = _roleManager.Roles.Select(i => i.Name);
+                ViewBag.Roles = roles2;
+                return View(model);
+            }
+            var roles3 = _roleManager.Roles.Select(i => i.Name);
+            ViewBag.Roles = roles3;
+            return View(model);
+        }
+        public IActionResult RoleList()
+        {
+            return View(_roleManager.Roles);
+        }
+
+
+        public IActionResult RoleCreate()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> RoleCreate(RoleModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _roleManager.CreateAsync(
+                    new IdentityRole(model.Name)
+                    );
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("RoleList");
+                }
+
+                //Geri kalan kısmına yarın devam edeceğiz.
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+            return View(model);
+        }
+
+        public async Task<IActionResult> RoleEdit(string id)
+        {
+            var role = await _roleManager.FindByIdAsync(id);
+            var members = new List<User>();
+            var nonMembers = new List<User>();
+
+            foreach (var user in _userManager.Users)
+            {
+                //if (await _userManager.IsInRoleAsync(user,role.Name))
+                //{
+                //    members.Add(user);
+                //}
+                //else
+                //{
+                //    nonMembers.Add(user);
+                //}
+
+                var list = await _userManager.IsInRoleAsync(user, role.Name) ? members : nonMembers;
+                list.Add(user);
+            }
+            var model = new RoleDetails()
+            {
+                Role = role,
+                Members = members,
+                NonMembers = nonMembers
+            };
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> RoleEdit(RoleEditModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                foreach (var userId in model.IdsToAdd ?? new string[] { })
+                {
+                    var user = await _userManager.FindByIdAsync(userId);
+                    if (user != null)
+                    {
+                        var result = await _userManager.AddToRoleAsync(user, model.RoleName);
+                        if (!result.Succeeded)
+                        {
+                            foreach (var error in result.Errors)
+                            {
+                                ModelState.AddModelError("", error.Description);
+                            }
+                        }
+                    }
+                }
+
+                foreach (var userId in model.IdsToDelete ?? new string[] { })
+                {
+                    var user = await _userManager.FindByIdAsync(userId);
+                    if (user != null)
+                    {
+                        var result = await _userManager.RemoveFromRoleAsync(user, model.RoleName);
+                        if (!result.Succeeded)
+                        {
+                            foreach (var error in result.Errors)
+                            {
+                                ModelState.AddModelError("", error.Description);
+                            }
+                        }
+                    }
+                }
+            }
+            return Redirect("/admin/role/" + model.RoleId);
         }
         public IActionResult Index()
         {
@@ -61,7 +271,7 @@ namespace DurkayaRecipe.WebUI.Controllers
                     IsApproved = model.IsApproved,
                     IsHome = model.IsHome
                 };
-                _foodService .Create(food, categoryIds);
+                _foodService.Create(food, categoryIds);
 
                 CreateMessage("Ürün başarıyla eklenmiştir", "success");
                 return RedirectToAction("FoodList");
