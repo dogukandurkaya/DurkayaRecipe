5958bbf94097e87ff9d1a78699ec89ca4b31de3a
dogukandurkaya <98876145+dogukandurkaya@users.noreply.github.com>
2022-06-23 22:10:51 +0300

    23.06.2022

--- DurkayaRecipe.WebUI/EmailService/IEmailSender.cs
+++ DurkayaRecipe.WebUI/EmailService/IEmailSender.cs
@@ -7,8 +7,6 @@ namespace DurkayaRecipe.WebUI.EmailService
 {
     public interface IEmailSender 
     {
-        //Bu interface farklı mail server tekniklerine göre mail gönderme 
-        //metotlarımızı yapılandırabilmek için genel olarak tasarlanmıştır.
         Task SendEmailAsync(string email, string subject, string htmlMessage);
     }
 }
